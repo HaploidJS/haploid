c4c4a4461455de2fdf4ffe2ed62858cf5ecd005d
yanni4night <yanni4night@gmail.com>
2023-10-28 00:36:54 +0800

    chore: update README.md and copyright

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -25,7 +25,7 @@ function generateBanner(filename) {
     return `/** @license ${packageJson.name.split('/').pop()} v${packageJson.version}
 * ${filename}
 *
-* Copyright (c) Kuaishou
+* Copyright (c) kuaishou.com
 *
 * This source code is licensed under the MIT license found in the
 * LICENSE file in the root directory of this source tree.
