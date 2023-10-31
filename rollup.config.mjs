7a961501a2ffb8f42af5b4b6f5647f8f255d39aa
yanni4night <yanni4night@gmail.com>
2023-10-31 16:36:08 +0800

    chore: update license

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -25,7 +25,7 @@ function generateBanner(filename) {
     return `/** @license ${packageJson.name.split('/').pop()} v${packageJson.version}
 * ${filename}
 *
-* Copyright (c) kuaishou.com
+* Copyright (c) Kwai, Inc
 *
 * This source code is licensed under the MIT license found in the
 * LICENSE file in the root directory of this source tree.
